c9d9a58ffd3d833deea8d76225cddd29dced7de5
KyoZza <sandro.scheffmann@hotmail.de>
2025-03-01 21:35:44 +0900

    add greeting form to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
+import VButton from '@/components/button'
+import VInputText from '@/components/inputText'
 import VPage from '@/components/page'
+import { ref } from 'vue'
+
+const name = ref('')
+const greeting = ref('')
+
+const onSubmit = () => {
+  greeting.value = `Hello, ${name.value}!`
+}
 </script>
 
 <template>
-  <VPage title="Home"> </VPage>
+  <VPage title="Home">
+    <div class="flex flex-col gap-4">
+      <form class="flex gap-2" @submit.prevent="onSubmit">
+        <VInputText v-model="name" placeholder="Enter your name" data-testid="form-input-name" />
+        <VButton label="Greet" :disabled="!name" type="submit" data-testid="form-submit" />
+      </form>
+
+      <p data-testid="greeting-text">{{ greeting }}</p>
+    </div>
+  </VPage>
 </template>
