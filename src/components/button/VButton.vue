cfc294d1037c3b087ba12814f4df2562ba047810
KyoZza <sandro.scheffmann@hotmail.de>
2025-03-01 20:31:59 +0900

    add border go to VButton

--- src/components/button/VButton.vue
+++ src/components/button/VButton.vue
@@ -7,12 +7,16 @@ defineEmits<ButtonEmits>()
 
 <template>
   <button
-    class="rounded-sm"
+    class="rounded-sm border"
     :class="{
-      'bg-teal-600 hover:bg-teal-500 text-white': variant === 'primary',
-      'bg-slate-300 hover:bg-slate-200 ': variant === 'secondary',
-      'bg-emerald-600 hover:bg-emerald-500 text-white': variant === 'success',
-      'bg-rose-600 hover:bg-rose-500 text-white bg-': variant === 'danger',
+      'bg-teal-600 border-teal-600 hover:bg-teal-500 hover:border-teal-500 text-white':
+        variant === 'primary',
+      'bg-slate-300 borderg-slate-300 hover:bg-slate-200 hover:border-slate-200 ':
+        variant === 'secondary',
+      'bg-emerald-600 border-emerald-600 hover:bg-emerald-500 hover:border-emerald-500 text-white':
+        variant === 'success',
+      'bg-rose-600 border-rose-600 hover:bg-rose-500 hover:border-rose-500 text-white bg-':
+        variant === 'danger',
       'text-sm px-2 py-0.5': size === 'sm',
       'text-base px-3 py-1': size === 'md',
       'text-lg px-4 py-1.5': size === 'lg',
